4951a785a294adfae5214bf28f47e84729d1416c
Tripl0Color <62907882+Tripl0Color@users.noreply.github.com>
2020-04-11 22:39:54 +0300

    Сжатие кода,убрана проверка токена (зачем она на основной странице?)
    "(зачем она на основной странице?)" 
    Можно же быстро проверить робит\нет

--- main.py
+++ main.py
@@ -1,36 +1,10 @@
 import requests
 import time
 
-TOKEN = '' 
-
-
-def check_token(token): # проверяем токен
-	try:
-		response = requests.post('https://api.vk.com/method/users.setCovidStatus?' \
-		'api_id=7362610'\
-		'&method=users.setCovidStatus&format=json&' \
-		'v=5.103' \
-		'&status_id=1' + \
-		'&access_token=' + str(token) + \
-		'&request_id=5')
-
-		print('Проверка токена..')
-		data = response.json()
-		if data['response']: # проверяем появится ли ошибка
-			pass
-
-		return True
-
-	except:
-		return False	
-
+token = 'pastetokenhere' 
 
 def requests_smile(token):
 	try:
-		if check_token(token) == True:
-			print('Токен верный..')
-
-			print('Скрипт запущен')
 			while True:
 				for item in range(1, 18):
 					response = requests.post('https://api.vk.com/method/users.setCovidStatus?' \
@@ -40,9 +14,8 @@ def requests_smile(token):
 						'&status_id=' + str(item) + \
 						'&access_token=' + str(TOKEN) + \
 						'&request_id=5')
-				time.sleep(0.1)
-		else:
-			print('Токен введен неверно')
+				time.sleep(1)
+		
 
 	except Exception as error:
 		print(f'[ОШИБКА] {error}')
